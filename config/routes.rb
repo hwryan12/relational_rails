b2c99211ee3fa3f0b5a05b0686641bd28001d0c3
Harrison Ryan <hwryan12@gmail.com>
2023-02-02 20:12:26 -0600

    feature: Complete user story 4

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/companies', to: 'companies#index'
   get '/companies/:id', to: 'companies#show'
   get '/vessels', to: 'vessels#index'
+  get '/vessels/:id', to: 'vessels#show'
 end
